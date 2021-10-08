41568f1a3b3b37e25f0208994263b77ff1ee953a
AmaniAbidi <91508141+AmaniAbidi@users.noreply.github.com>
2021-10-08 01:37:15 -0600

    Add files via upload

--- tests.js
+++ tests.js
@@ -1,12 +1,12 @@
 describe("filter", function () {
-    it("an String method that takes an array of string arguments, and returns the filtered string without thoses string inputs",
+    it("a String method that accepts an array of string arguments, and returns the filtered string without thoses string inputs",
         function () {
             assert.equal("This house is not nice!".filter('not'), "This house is nice!");
         });
 });
 
 describe("bubbleSort", function () {
-    it("an Array method, returns the array sorted using bubble sort algo",
+    it("an Array method, returns the array sorted using bubble sort",
         function () {
             assert.equal([7,5,0, 3,-4,1].bubbleSort().toString(),  [-4, 0, 1, 3, 5, 7].toString());
         });
